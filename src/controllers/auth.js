84708f19ced420b85bb710219ce9d80f55427c1f
lenovo <yosepalexsander9@gmail.com>
2021-07-15 20:05:02 +0700

    done

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,97 +1,11 @@
-// import model
-const { user } = require("../../models");
+// import model here
 
-// import joi validation
-const Joi = require("joi");
+// import package here
 
 exports.register = async (req, res) => {
-  // our validation schema here
-  const schema = Joi.object({
-    name: Joi.string().min(5).required(),
-    email: Joi.string().email().min(6).required(),
-    password: Joi.string().min(6).required(),
-  });
-
-  // do validation and get error object from schema.validate
-  const { error } = schema.validate(req.body);
-
-  // if error exist send validation error message
-  if (error)
-    return res.status(400).send({
-      error: {
-        message: error.details[0].message,
-      },
-    });
-
-  try {
-    const user = await user.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    res.status(200).send({
-      status: "success...",
-      data: {
-        name: user.name,
-        email: user.email,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      status: "failed",
-      message: "Server Error",
-    });
-  }
+  // code here
 };
 
 exports.login = async (req, res) => {
-  // our validation schema here
-  const schema = Joi.object({
-    email: Joi.string().email().min(6).required(),
-    password: Joi.string().min(6).required(),
-  });
-
-  // do validation and get error object from schema.validate
-  const { error } = schema.validate(req.body);
-
-  // if error exist send validation error message
-  if (error)
-    return res.status(400).send({
-      error: {
-        message: error.details[0].message,
-      },
-    });
-
-  try {
-    const userExist = await user.findOne({
-      where: {
-        email: req.body.email,
-      },
-      attributes: {
-        exclude: ["createdAt", "updatedAt"],
-      },
-    });
-
-    if (userExist.password !== req.body.password) {
-      return res.status(400).send({
-        status: "failed",
-        message: "credential is invalid",
-      });
-    }
-    res.status(200).send({
-      status: "success...",
-      data: {
-        name: userExist.name,
-        email: userExist.email
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      status: "failed",
-      message: "Server Error",
-    });
-  }
+  // code here
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,35 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
-const { addUsers, getUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
-const { getProduct, addProduct } = require('../controllers/product')
-const { getTransactions, addTransaction } = require('../controllers/transaction')
-const { register, login } = require('../controllers/auth')
+const {
+  addUsers,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+const { getProduct, addProduct } = require("../controllers/product");
+const {
+  getTransactions,
+  addTransaction,
+} = require("../controllers/transaction");
+// import controller here
 
 // Route
-router.post('/user', addUsers)
-router.get('/users', getUsers)
-router.get('/user/:id', getUser)
-router.patch('/user/:id', updateUser)
-router.delete('/user/:id', deleteUser)
+router.post("/user", addUsers);
+router.get("/users", getUsers);
+router.get("/user/:id", getUser);
+router.patch("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
 
-router.get('/products', getProduct)
-router.post('/product', addProduct)
+router.get("/products", getProduct);
+router.post("/product", addProduct);
 
-router.get('/transactions', getTransactions)
-router.post('/transaction', addTransaction)
+router.get("/transactions", getTransactions);
+router.post("/transaction", addTransaction);
 
-router.post('/register', register)
-router.post('/login', login)
+// add route here
 
-module.exports = router
+module.exports = router;
