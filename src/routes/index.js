f1320fef57a23b5d39dd811057a0676d231f0039
lenovo <yosepalexsander9@gmail.com>
2021-07-15 11:31:18 +0700

    done

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Controller
+const { addUsers, getUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
+const { getProduct, addProduct } = require('../controllers/product')
+const { getTransactions, addTransaction } = require('../controllers/transaction')
+const { register, login } = require('../controllers/auth')
+
+// Route
+router.post('/user', addUsers)
+router.get('/users', getUsers)
+router.get('/user/:id', getUser)
+router.patch('/user/:id', updateUser)
+router.delete('/user/:id', deleteUser)
+
+router.get('/products', getProduct)
+router.post('/product', addProduct)
+
+router.get('/transactions', getTransactions)
+router.post('/transaction', addTransaction)
+
+router.post('/register', register)
+router.post('/login', login)
+
+module.exports = router
